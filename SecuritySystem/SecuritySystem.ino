1af434a8e6851d48bcc2c5f642f557de0b5ba62d
Alexander Ortiz <ludwingaelxander230@gmail.com>
2024-02-03 14:23:15 -0600

    INGRESO DE VARIABLES POR MEDIO DEL TECLADO

--- SecuritySystem/SecuritySystem.ino
+++ SecuritySystem/SecuritySystem.ino
@@ -6,16 +6,16 @@ const byte Columnas = 4;
 
 char Teclado[Filas][Columnas] = {
 
-{'1','2','3','+'},
-{'4','5','6','-'},
-{'7','8','9','x'},
-{'*','0','#','/'}
+{'7','8','9','A'},
+{'4','5','6','B'},
+{'1','2','3','C'},
+{'*','0','#','D'}
 };
 
 byte Fpines[Filas] = {5,4,3,2};
 byte Cpines[Columnas] = {11,10,9,8};
 
-Keypad Teclado = Keypad(makeKeymap(Teclado), Fpines, Cpines, Filas, Columnas);
+Keypad teclado = Keypad(makeKeymap(Teclado), Fpines, Cpines, Filas, Columnas);
 
 void setup() {
   
@@ -24,6 +24,17 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
+  String Pasword = "AC6C124";
+  
+  char Ingreso = teclado.getKey();
+
+  if (Ingreso){
+     Serial.print(Pasword);
+    Serial.print("INGRESE LA CONTRASEA: ");
+    Serial.print("tecla ingresada: ");
+    Serial.print(Ingreso);
+  }
+  
 }
